e3c16740aed876e2b4301b7221b354cbf5a0081b
Eason0628 <zhaowintoo@gmail.com>
2025-10-08 20:32:21 +0800

    4.AddLoginAndRegisterPages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-// function App() {
-//   return (
-//     <div className="App">
-//     </div>
-//   );
-// }
-// export default App;
-
 import 'normalize.css';
-import './styles/base.css';
 import './styles/border.css';
+import './styles/base.css';
+
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Guide from './containers/Guide';
 import Login from './containers/Login';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import Register from './containers/Register';
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path='/' element={<Guide />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Guide/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
       </Routes>
     </HashRouter>
-  );
+  )
 }
 
 export default App;
--- src/containers/Guide/index.tsx
+++ src/containers/Guide/index.tsx
@@ -1,24 +1,19 @@
-import './style.css';
 import { useEffect, useRef } from 'react';
+import './style.scss';
 import { useNavigate } from 'react-router-dom';
 
-const useRefAnimation = () => {
-  // 处理动画相关的逻辑,页面加载完成后，guide-page添加opacity 动画 0 -> 1
+const Guide = () => {
+  // 处理动画相关的逻辑
   const ref = useRef<HTMLDivElement>(null!);
   useEffect(() => {
     ref.current.style.opacity = '1';
   }, [])
-  return ref;
-}
-
-const Guide = () => {
-  const ref = useRefAnimation();
 
   // 处理页面跳转相关的逻辑
   const navigate = useNavigate();
   function handleIconClick() {
     navigate('/login');
-  }
+  };
 
   return (
     <div ref={ref} className="page guide-page">
@@ -33,7 +28,10 @@ const Guide = () => {
         className="sub-pic"
         src={require('../../images/slogn_word_icon_@2x.png')}
       />
-      <div className="iconfont arrow-icon" onClick={handleIconClick}>&#xe60c;</div>
+      <div
+        className="iconfont arrow-icon"
+        onClick={handleIconClick}
+      >&#xe60c;</div>
     </div>
   )
 }
