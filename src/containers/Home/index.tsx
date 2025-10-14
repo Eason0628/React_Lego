14efd4377a70e11b94e747d8ab661d4d179cccca
Eason0628 <zhaowintoo@gmail.com>
2025-10-14 22:33:55 +0800

    7.6 Split the Home component into multiple components

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -2,13 +2,14 @@ import 'swiper/css';
 import './style.scss';
 import type { ResponseType } from './types';
 import { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import useRequest from '../../hooks/useRequest';
+import Banner from './components/Banner';
+import Category from './components/Category';
+import Card from './components/Card';
 
 const localLocation = localStorage.getItem('location');
-const locationHistory = localLocation ? JSON.parse(localLocation): null;
+const locationHistory = localLocation ? JSON.parse(localLocation) : null;
 
-// 默认请求数据
 // 默认请求数据
 const a1 = 'APP-UvygAWn-4519950447516193282-2';
 const a2 = 'KEY035UvyhbsvXDuoopaM2b3H4jRRnjnBpt53gOXsdbj3';
@@ -26,12 +27,12 @@ const defaultRequestData = {
 }
 
 const Home = () => {
-  const [ requestData, setRequestData ] = useState(defaultRequestData);
+  const [requestData, setRequestData] = useState(defaultRequestData);
   const { data } = useRequest<ResponseType>(requestData);
 
   // 获取经纬度信息
   useEffect(() => {
-    if(navigator.geolocation && !locationHistory) {
+    if (navigator.geolocation && !locationHistory) {
       console.log('get location');
       navigator.geolocation.getCurrentPosition((position) => {
         const { coords } = position;
@@ -41,191 +42,47 @@ const Home = () => {
         }));
         setRequestData({
           ...defaultRequestData,
-          data: { a1, a2, t1, latitude, longitude } 
+          data: { a1, a2, t1, latitude, longitude }
         });
       }, (error) => {
         console.log(error);
-      }, {timeout: 500})
+      }, { timeout: 500 })
     }
   }, []);
 
-  const [ page, setPage ] = useState(1);
+  let location, banners, categories, freshes = undefined;
+  const dataResult = data?.data;
+  if (dataResult) {
+    location = dataResult.location;
+    banners = dataResult.banners;
+    categories = dataResult.categories;
+    freshes = dataResult.freshes;
+  }
+
   return (
     <div className='page home-page'>
-      <div className='banner'>
-        <h3 className='location'>
-          <span className='iconfont'>&#xe67c;</span>
-          {data?.data.location.address || ''}
-        </h3>
-        <div className='search'>
-          <span className='iconfont'>
-            &#xe64e;
-          </span>
-          请输入你需要搜索的内容
-        </div>
-        <div className='swiper-area'>
-          <Swiper
-            spaceBetween={0}
-            slidesPerView={1}
-            onSlideChange={(e) => setPage(e.activeIndex + 1)}
-          >
-            {
-              (data?.data.banners || []).map(item => {
-                return (
-                  <SwiperSlide key={item.id}>
-                    <div className='swiper-item'>
-                      <img className='swiper-item-img' src={item.url} alt='轮播图'/>
-                    </div>
-                  </SwiperSlide>
-                )
-              })
-            }
-          </Swiper>
-          <div className='pagination'>{page}/{data?.data.banners.length || 0}</div>
-        </div>
+      <Banner location={location} banners={banners} />
+      <Category categories={categories} />
+      <Card title='新品尝鲜' list={freshes} />
+      <div className='bottom'>
+        - 我是有底线的 -
       </div>
-      <div className='category'>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
+      <div className='docker'>
+        <div className='docker-item docker-item-active'>
+          <p className='iconfont docker-item-icon'>&#xe600;</p>
+          <p className='docker-item-title'>首页</p>
         </div>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
+        <div className='docker-item'>
+          <p className='iconfont docker-item-icon'>&#xe60d;</p>
+          <p className='docker-item-title'>分类</p>
         </div>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
+        <div className='docker-item'>
+          <p className='iconfont docker-item-icon'>&#xe6af;</p>
+          <p className='docker-item-title'>购物车</p>
         </div>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
-        </div>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
-        </div>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
-        </div>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
-        </div>
-        <div className='category-item'>
-          <img
-            className='category-item-img'
-            alt='新鲜蔬菜'
-            src='http://statics.dell-lee.com/shopping/category-1.png'
-          />
-          <p className='category-item-desc'>新鲜蔬菜</p>
-        </div>
-      </div>
-      <div className='card'>
-        <h3 className='card-title'>
-          <img
-            alt='新品尝鲜'
-            className='card-title-img'
-            src='http://statics.dell-lee.com/shopping/hot.png'
-          />
-          新品尝鲜
-          <div className='card-title-more'>更多<span className='iconfont'>&#xe613;</span></div>
-        </h3>
-        <div className='card-content'>
-          <div className='card-content-item'>
-            <img
-              alt=''
-              className='card-content-item-img'
-              src='http://statics.dell-lee.com/shopping/hot.png'
-            />
-            <p className='card-content-item-desc'>金锣 国产猪肉 去皮猪五花肉块 …</p>
-            <p className='card-content-item-price'>
-              <span className='card-content-item-yen'>&yen;</span>
-              66.9
-              <div className='iconfont'>&#xe7e0;</div>
-            </p>
-          </div>
-          <div className='card-content-item'>
-            <img
-              alt=''
-              className='card-content-item-img'
-              src='http://statics.dell-lee.com/shopping/hot.png'
-            />
-            <p className='card-content-item-desc'>金锣 国产猪肉 去皮猪五花肉块 …</p>
-            <p className='card-content-item-price'>
-              <span className='card-content-item-yen'>&yen;</span>
-              66.9
-              <div className='iconfont'>&#xe7e0;</div>
-            </p>
-          </div>
-          <div className='card-content-item'>
-            <img
-              alt=''
-              className='card-content-item-img'
-              src='http://statics.dell-lee.com/shopping/hot.png'
-            />
-            <p className='card-content-item-desc'>金锣 国产猪肉 去皮猪五花肉块 …</p>
-            <p className='card-content-item-price'>
-              <span className='card-content-item-yen'>&yen;</span>
-              66.9
-              <div className='iconfont'>&#xe7e0;</div>
-            </p>
-          </div>
-          <div className='card-content-item'>
-            <img
-              alt=''
-              className='card-content-item-img'
-              src='http://statics.dell-lee.com/shopping/hot.png'
-            />
-            <p className='card-content-item-desc'>金锣 国产猪肉 去皮猪五花肉块 …</p>
-            <p className='card-content-item-price'>
-              <span className='card-content-item-yen'>&yen;</span>
-              66.9
-              <div className='iconfont'>&#xe7e0;</div>
-            </p>
-          </div>
-          <div className='card-content-item'>
-            <img
-              alt=''
-              className='card-content-item-img'
-              src='http://statics.dell-lee.com/shopping/hot.png'
-            />
-            <p className='card-content-item-desc'>金锣 国产猪肉 去皮猪五花肉块 …</p>
-            <p className='card-content-item-price'>
-              <span className='card-content-item-yen'>&yen;</span>
-              66.9
-              <div className='iconfont'>&#xe7e0;</div>
-            </p>
-          </div>
+        <div className='docker-item'>
+          <p className='iconfont docker-item-icon'>&#xe660;</p>
+          <p className='docker-item-title'>我的</p>
         </div>
       </div>
     </div>
